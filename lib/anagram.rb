127d7c22dd4f6b9a4eb8d5c9c6f5024f1108a1a5
onyangoouma <jacksonouma04@gmail.com>
2022-08-26 00:20:50 +0300

    commit tree 87ee624f1d32ad64073c3724ce81119007619e3a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initalize(word)
+        @name = word
+    end
+
+    def match(array)
+        array.select {|x| x.split("").sort ==@name.split("").sort}
+    end
+end
